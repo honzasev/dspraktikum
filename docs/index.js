b36626fe86b80e738c241011dd5dd28f1555dde6
honzasev <72665642+honzasev@users.noreply.github.com>
2021-10-14 11:48:37 +0200

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -12,7 +12,7 @@ async function app() {
   console.log('Loading mobilenet..');
 
   // Load the model.
-  net = await tf.loadLayersModel('https://raw.githubusercontent.com/simecek/dspracticum2021/master/docs/export_model/model.json');
+  net = await tf.loadLayersModel('https://raw.githubusercontent.com/honzasev/dspraktikum/main/docs/export_model/model.json');
   console.log('Successfully loaded model');
 
   // Make a prediction through the model on our image.
